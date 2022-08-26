a711c039271ac35a990589a4f17ec18649aad708
IndraP24 <indrashis985@gmail.com>
2022-08-26 20:39:45 +0530

    Add Reverse num practice file

---
+++ src/STRIVER/DAY0_Misc/ReverseNum.java
@@ -0,0 +1,41 @@
+package STRIVER.DAY0_Misc;
+
+import java.util.*;
+
+// Given a signed 32-bit integer x, return x with its digits reversed.
+// If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// https://leetcode.com/problems/reverse-integer/
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        if (n < 0) {
+            System.out.println(-reverse(-n));
+            return;
+        }
+
+        System.out.println(reverse(n));
+
+    }
+
+    public static int reverse(int n) {
+        int rev = 0;
+
+        while (n != 0) {
+            int r = n % 10;
+            n /= 10;
+
+            if (rev > Integer.MAX_VALUE/10 || rev == Integer.MAX_VALUE/10 && r > 7)
+                return 0;
+
+            if (rev < Integer.MIN_VALUE/10 || rev == Integer.MIN_VALUE/10 && r < -8)
+                return 0;
+
+            rev = rev * 10 + r;
+        }
+
+        return rev;
+    }
+}
