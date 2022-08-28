61aab5d38ecf2cba6a398fcb9697cb07e6311b16
IndraP24 <indrashis985@gmail.com>
2022-08-28 23:56:14 +0530

    Add initial Medium Arrays solution file

--- src/STRIVER/DAY1_Arrays_I/A1_SetMatrixZero.java
+++ src/STRIVER/DAY1_Arrays_I/A1_SetMatrixZero.java
@@ -4,4 +4,17 @@ public class A1_SetMatrixZero {
     public static void main(String[] args) {
 
     }
+
+    public static void setZeroes(int[][] matrix) {
+        int rows = matrix.length, cols = matrix[0].length, flag = 0;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (matrix[row][col] == 0) {
+                    flag = 1;
+                    
+                }
+            }
+        }
+    }
 }
