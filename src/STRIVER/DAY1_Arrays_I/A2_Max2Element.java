392f8b536989ff23e26bc7d3be4a3c94f6548eec
IndraP24 <indrashis985@gmail.com>
2022-08-29 23:57:07 +0530

    Add Easy Arrays 2nd largest solution file

---
+++ src/STRIVER/DAY1_Arrays_I/A2_Max2Element.java
@@ -0,0 +1,42 @@
+package STRIVER.DAY1_Arrays_I;
+
+public class A2_Max2Element {
+    public static void main(String[] args) {
+        int[] A = {1, 8, 101, 7, 56, 90};
+        System.out.println(secondLargest(A, A.length));
+        System.out.println(secondLargestEA(A, A.length));
+    }
+
+    static int secondLargest(int[] arr, int n) {
+        int max = 0;
+        for (int i=0; i<n; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        int max2 = 0;
+        for (int i=0; i<n; i++) {
+            if (arr[i] != max && arr[i] > max2)
+                max2 = arr[i];
+        }
+
+        return max2;
+    }
+
+    static int secondLargestEA(int[] arr, int n) {
+        int i, first, sec;
+        first = sec = -1;
+
+        if (n < 2)
+            return sec;
+
+        for (i=0; i<n; i++) {
+            if (arr[i] > first) {
+                sec = first;
+                first = arr[i];
+            } else if (arr[i] > sec && arr[i] != first) {
+                sec = arr[i];
+            }
+        }
+        return sec;
+    }
+}
